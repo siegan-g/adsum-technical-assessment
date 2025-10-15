1206db45ef2931ec704d635f827b6a44885e5ecc
siegan-g <siegang@ec3.co.za>
2025-10-15 14:32:16 +0200

    add cli presentation layer

--- backend/presentation/api/main.py
+++ backend/presentation/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routes.main import api_router
-from application.dependency_container import get_settings,get_seeder_service
+from application.dependency_container import get_settings
 
 import uvicorn
 
@@ -13,7 +13,6 @@ app = FastAPI(title=app_settings["title"], version=app_settings["version"])
 app.include_router(api_router, prefix=app_settings["prefix"])
 
 if __name__ == "__main__":
-    get_seeder_service().seed(num_invoices=30,num_payments=30)
     uvicorn.run("presentation.api.main:app", host=app_settings["host"], port=app_settings["port"])
 
 
