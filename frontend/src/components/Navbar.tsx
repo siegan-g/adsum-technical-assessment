6771092c52f557a8c0058052ba8cf5ef3dceeea2
siegan-g <siegangovender@outlook.com>
2025-10-16 20:15:21 +0200

    remove unused default avatar img

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -124,7 +124,7 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Administrator" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Administrator" src="" />
               </IconButton>
             </Tooltip>
             <Menu
