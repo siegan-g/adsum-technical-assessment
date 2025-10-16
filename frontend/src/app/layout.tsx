347acc1f94fed506b36d3b23150f3e1f7f279bf9
siegan-g <siegangovender@outlook.com>
2025-10-16 16:07:07 +0200

    Add responsive MUI navbar and update homepage

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import ResponsiveAppBar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <ResponsiveAppBar/>
         {children}
       </body>
     </html>
