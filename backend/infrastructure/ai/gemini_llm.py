ea51a26f9a6f49d93ae54792516a684140ff001f
siegan-g <siegangovender@outlook.com>
2025-10-17 00:20:50 +0200

    add instructions to gemini

--- backend/infrastructure/ai/gemini_llm.py
+++ backend/infrastructure/ai/gemini_llm.py
@@ -1,10 +1,12 @@
 from infrastructure.ai.llm import Llm
 from google import genai
+from google.genai.types import GenerateContentConfig
 
 class GeminiLlm(Llm):
-    def __init__(self,api_key:str)->None: 
+    def __init__(self,instructions:str,api_key:str)->None: 
         self.client = genai.Client(api_key=api_key)
+        self.instructions = instructions
         
     def generate_text(self, message: str) -> str | None:
-        response = self.client.models.generate_content(model="gemini-2.5-flash",contents=message) # type: ignore
+        response = self.client.models.generate_content(model="gemini-2.5-flash",contents=message,config=GenerateContentConfig(system_instruction=self.instructions)) # type: ignore
         return response.text
